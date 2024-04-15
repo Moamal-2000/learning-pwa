e6bf7bf10f4e7f54d610f0cb03bd7db5e7a423e5
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-04-15 20:47:21 +0300

    test pwa #3

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Variables
-const CACHE_NAME = "PWA-v2";
+const CACHE_NAME = "PWA-v3";
 const ASSETS = ["/", "/index.html", "/manifest.json"];
 
 // Functions
@@ -16,9 +16,6 @@ async function updateCacheVersion() {
     (cacheName) => cacheName !== CACHE_NAME
   );
 
-  console.log("cachesNames", cachesNames);
-  console.log("cachesToRemove", cachesToRemove);
-
   return Promise.all(
     cachesToRemove.map((cacheName) => caches.delete(cacheName))
   );
@@ -29,14 +26,14 @@ async function getResponseFromCacheOrFetch(fetchEvent) {
   const cachedResponse = await caches.match(request);
   const fetchedResponse = await fetch(request);
 
-  // if (!cachedResponse) {
-  //   const isGoogleExtension = request.url.includes("chrome-extension");
-  //   const cache = await caches.open(CACHE_NAME);
+  if (!cachedResponse) {
+    const isGoogleExtension = request.url.includes("chrome-extension");
+    const cache = await caches.open(CACHE_NAME);
 
-  //   if (!isGoogleExtension) {
-  //     cache.put(request, fetchedResponse.clone());
-  //   }
-  // }
+    if (!isGoogleExtension) {
+      cache.put(request, fetchedResponse.clone());
+    }
+  }
 
   return cachedResponse ? cachedResponse : fetchedResponse;
 }
